6e976d2a51d09ee67093ceef155edd4d3bbf3810
guilhernunes <guilherondonnunes@gmail.com>
2026-02-04 20:43:47 -0400

    alterando limite para 100

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // //let paragrafo = document.querySelector('p');
 // //paragrafo.innerHTML = 'Escolha um numero entre 1 a 10';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = geradorDeNumero();
 let tentativas = 1;
 function exibirTextoNaTela(tag, texto){
@@ -15,7 +15,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial() { 
     exibirTextoNaTela('h1', 'Jogo do número secreto'); 
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10'); I
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100'); I
 
 }
 exibirMensagemInicial();
